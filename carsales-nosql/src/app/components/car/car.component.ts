d6ba0a45ccd8d6dcc77016cc732d7ce7d0ead4a3
fergalfeeney <98536841+fergalfeeney@users.noreply.github.com>
2023-04-10 18:02:08 +0100

    Done but not working correctly

--- carsales-nosql/src/app/components/car/car.component.ts
+++ carsales-nosql/src/app/components/car/car.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { CarApiService } from 'src/app/services/car-api.service';
 import { ICar } from 'src/app/interfaces/car';
 import { Output } from '@angular/core';
@@ -11,11 +10,13 @@ import { EventEmitter } from '@angular/core';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent {
+  @Output() carDeletedEvent = new EventEmitter<string>()
+  
   @Input()
   carData!: ICar;
 carImageWidth: number = 300;
 
-@Output() carDeletedEvent = new EventEmitter<string>()
+
 
 constructor(private _carAPIService: CarApiService) { }
 
--- carsales-nosql/src/app/services/car-api.service.ts
+++ carsales-nosql/src/app/services/car-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from  '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError, tap } from 'rxjs';
+import { catchError, Observable, tap } from 'rxjs';
 import {ICar} from '../interfaces/car';
 
 
